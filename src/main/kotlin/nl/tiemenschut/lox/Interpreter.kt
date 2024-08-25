4c02e3a21b9c3ce9eb0f7a2bfd4df65ee04b0ce7
Tiemen Schut <tschut@gmail.com>
2024-08-25 14:39:37 +0200

    Chapter 7

--- src/main/kotlin/nl/tiemenschut/lox/Interpreter.kt
+++ src/main/kotlin/nl/tiemenschut/lox/Interpreter.kt
@@ -26,6 +26,7 @@ class Interpreter : Expression.Visitor<Any> {
                     (left as Double) - (right as Double)
                 }
                 SLASH -> {
+                    if (right is Double && right == 0.0) throw RuntimeError(expression.operator, "Division by zero.")
                     checkNumberOperands(expression.operator, left, right)
                     (left as Double) / (right as Double)
                 }
@@ -35,8 +36,8 @@ class Interpreter : Expression.Visitor<Any> {
                 }
                 PLUS -> {
                     if (left is Double && right is Double) (left + right)
-                    else if (left is String && right is String) ("$left$right")
-                    else throw RuntimeError(expression.operator, "Operands must be two numbers or two strings.")
+                    else if (left is String || right is String) ("${left.stringify()}${right.stringify()}")
+                    else throw RuntimeError(expression.operator, "Operands must be two numbers or one of them must be a string.")
                 }
                 GREATER -> {
                     checkNumberOperands(expression.operator, left, right)
@@ -84,7 +85,6 @@ class Interpreter : Expression.Visitor<Any> {
         }
     }
 
-
     private fun isEqual(a: Any?, b: Any?): Boolean = if (a == null) (b == null) else (a == b)
 
     private fun isTruthy(thing: Any?): Boolean = when (thing) {
@@ -92,11 +92,10 @@ class Interpreter : Expression.Visitor<Any> {
         is Boolean -> thing
         else -> true
     }
-
 }
 
 private fun Any?.stringify(): String {
     return if (this == null) "nil"
-    else if (this is Double) toString().substringBefore('.')
+    else if (this is Double) toString().let { if (it.endsWith(".0")) it.substringBefore('.') else it }
     else toString()
 }
